c30aecf2b11acb54e3466c811694d37b3d39b1dc
Andres Martinez <andresmartgonz@gmail.com>
2021-01-21 11:12:29 +0100

    dsg002

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import Button from '../Button/Button'
 
 export default function Modal({data, onClick}) {
+    console.log(data)
 
     const months = ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic']
 
@@ -16,12 +17,25 @@ export default function Modal({data, onClick}) {
                 <div className="row justify-content-center">
                     <div className="col-sm-6 col-12 modal-body">
                         <span className="close" onClick={onClick}></span>
-                        <h1>{data.name}</h1>
-                        <p className="discipline">{data.discipline}</p>
-                        <p><strong>Biopsia</strong> {data.details}</p>
-                        <hr />
-                        <p><span>Biopsia</span> {data.number}</p>
-                        <p><span>Fecha</span> {new Date(data.date).getDate()} {months[new Date(data.date).getMonth()]} {new Date(data.date).getFullYear()} </p>
+                        <div className="row">
+                            <div className="col-6">
+                                <span className="icon">
+                                    <h1>Biopsia</h1>
+                                    <div className="b-number">{data.number}</div>
+                                </span>
+                            </div>
+                            <div className="col-6 b-date text-right d-flex justify-content-end align-items-end">
+                                {new Date(data.date).getDate()} {months[new Date(data.date).getMonth()]} {new Date(data.date).getFullYear()}
+                            </div>
+                        </div>
+                        <div className="patient-info">
+                            <div className="row">
+                                <div className="col-4 name">{data.user[0].name}</div>
+                                <div className="col-4 text-center"><strong>CI</strong> {data.user[0].dni}</div>
+                                <div className="col-4 text-right"><strong>Edad</strong> {new Date().getFullYear() - new Date(data.user[0].birthdate).getFullYear()}</div>
+                            </div>
+                        </div>
+
                         <p><span>Referencia</span> {data.reference}</p>
                         <p><span>Material remitido</span> {data.material}</p>
                         <p><span>Diagnóstico clínico</span> {data.clinic_diagnosis}</p>
