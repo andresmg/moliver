f82d62a528f9c2be17d174759b720a62127a0c8e
Andres Martinez <andresmartgonz@gmail.com>
2021-11-29 13:37:39 +0100

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,19 @@
+import './Footer.css'
 import React from 'react'
 
 export default function Footer() {
     return (
-        <div>
-
-        </div>
+        <footer className="Footer container-fluid">
+            <div className="container">
+                <div className="row">
+                    <div className="col-12 col-sm-6">
+                        Todos los derechos reservados {new Date().getFullYear()} © by Margarita Oliver
+                    </div>
+                    <div className="col-12 col-sm-6 Footer__link">
+                        Diseño y programación por <a href="https://www.linkedin.com/in/andresmg/" target="_blank" rel="noopener noreferrer">Andrés Martínez</a>
+                    </div>
+                </div>
+            </div>
+        </footer>
     )
 }
