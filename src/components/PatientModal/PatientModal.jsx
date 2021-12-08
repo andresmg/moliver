16828f1cd61cdacc5d1f88aab2122d18f5870712
Andres Martinez <andresmartgonz@gmail.com>
2021-12-08 11:34:33 +0100

    Patient History Modal Styles

--- src/components/PatientModal/PatientModal.jsx
+++ src/components/PatientModal/PatientModal.jsx
@@ -23,19 +23,24 @@ export default function PatientModal({patient, onClick}) {
         <>
             <div className={closeModal ? "modal PatientModal close-modal" : "modal PatientModal"}>
                 <div className="container">
-                    <div className="row justify-content-center">
-                        <div className="col-11 modal-body">
+                    <div className="row justify-content-end">
+                        <div className="col-2 PatientModal__addHistory">Añadir historia</div>
+                        <div className="col-12 modal-body">
                             <span className="close" onClick={onClick}></span>
                             <p className="PatientModal__patient">{patient.name} <strong>{patient.dni}</strong></p>
                             {patientHistories.map(el =>
                                 <>
-                                    <div className="patient-history">
-                                        <p className="PatientModal__date"><small>Consulta del </small>{new Date(el.date).getDate()} / {new Date(el.date).getMonth()} / {new Date(el.date).getFullYear()}</p>
-                                        <p className="PatientModal__reason"><small>Razón de la visita</small> {el.visit_reason}</p>
-                                        <p className="PatientModal__history"><small>Historia clínica</small> {el.clinic_history}</p>
-                                        <p className="PatientModal__diagnostic"><small>Diagnóstico</small> {el.diagnostics}</p>
-                                        <p className="PatientModal__treatment"><small>Tratamiento</small> {el.treatment}</p>
-                                    </div><hr></hr>
+                                    <div className="PatientModal__history">
+                                        <p className="PatientModal__date">Consulta del {new Date(el.date).getDate()} / {new Date(el.date).getMonth()} / {new Date(el.date).getFullYear()}</p>
+                                        <small>Razón de la visita</small>
+                                        <p className="PatientModal__reason"> {el.visit_reason}</p>
+                                        <small>Historia clínica</small>
+                                        <p className="PatientModal__clinic"> {el.clinic_history}</p>
+                                        <small>Diagnóstico</small>
+                                        <p className="PatientModal__diagnostic"> {el.diagnostics}</p>
+                                        <small>Tratamiento</small>
+                                        <p className="PatientModal__treatment"> {el.treatment}</p>
+                                    </div>
                                 </>
                             )}
                         </div>
